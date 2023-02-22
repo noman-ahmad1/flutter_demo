f5954aff59327c92263e21622f42a76b624e1978
noman-ahmad1 <nomanamjad105@gmail.com>
2023-02-22 17:05:46 +0500

    adding states

---
+++ lib/screens/email_auth/bloc/registration_bloc/register_state.dart
@@ -0,0 +1,15 @@
+
+import 'package:equatable/equatable.dart';
+
+class RegisterState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class RegisterInitialState extends RegisterState {}
+
+class RegisterValidState extends RegisterState {}
+
+class RegisterLoadingState extends RegisterState {}
+
+class RegisterErrorState extends RegisterState {}
