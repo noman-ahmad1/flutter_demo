e71e7dfef97fada2e189f9e2d75ffeabd4beb35b
noman-ahmad1 <nomanamjad105@gmail.com>
2023-03-02 23:59:57 +0500

    fixes

--- lib/screens/email_auth/sign_up_screen.dart
+++ lib/screens/email_auth/sign_up_screen.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:practice_application_1/screens/email_auth/bloc/sign_up_bloc/sign_up_event.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
