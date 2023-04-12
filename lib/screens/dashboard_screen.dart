537d4ab323395eced84fd900adb30f8ced2375a7
noman-ahmad1 <nomanamjad105@gmail.com>
2023-04-12 21:42:53 +0500

    new

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -31,7 +32,26 @@ class _DashboardScreenState extends State<DashboardScreen> {
           }, icon: const Icon(Icons.logout)) 
         ],
         title: const Text('Welcome To Dashboard'),
-      )
+      ),
+      body: const StepProgressIndicator(
+    totalSteps: 100,
+    currentStep: 32,
+    size: 8,
+    padding: 0,
+    selectedColor: Colors.yellow,
+    unselectedColor: Colors.cyan,
+    roundedEdges: Radius.circular(10),
+    selectedGradientColor: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Colors.yellowAccent, Colors.deepOrange],
+    ),
+    unselectedGradientColor: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Colors.black, Colors.blue],
+    ),
+),
     );
   }
 }
